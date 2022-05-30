ad74dd7849ff2df39de09eee1cc483a03f304fad
Mher <mherbadalyan@gmail.com>
2022-05-30 14:44:45 +0400

    added responses

--- src/main/java/com/example/storeautomation/controller/CategoryController.java
+++ src/main/java/com/example/storeautomation/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package com.example.storeautomation.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("store-automation")
 public class CategoryController {
+
 }
