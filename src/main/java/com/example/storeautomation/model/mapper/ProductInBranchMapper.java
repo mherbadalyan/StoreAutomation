74374c4b97c1597de6659944edb69e4c05f1c744
TigranTigranyan <T.tigranyan1993@mail.ru>
2022-05-30 14:01:17 +0400

    add BranchMapper, CategoryMapper, ProductInBranchMapper

--- src/main/java/com/example/storeautomation/model/mapper/ProductInBranchMapper.java
+++ src/main/java/com/example/storeautomation/model/mapper/ProductInBranchMapper.java
@@ -4,13 +4,26 @@ import com.example.storeautomation.model.dto.ProductInBranchDto;
 import com.example.storeautomation.model.entity.ProductInBranch;
 
 public class ProductInBranchMapper implements BaseMapper<ProductInBranch, ProductInBranchDto>{
+    private BranchMapper branchMapper;
+    private ProductMapper productMapper;
     @Override
     public ProductInBranch convertToEntity(ProductInBranchDto dto) {
-        return null;
+        ProductInBranch productInBranch =new ProductInBranch();
+        productInBranch.setId(dto.getId());
+        productInBranch.setQuantity(dto.getQuantity());
+        productInBranch.setDate(dto.getDate());
+        productInBranch.setBranch(branchMapper.convertToEntity(dto.getBranchDto()));
+        productInBranch.setProduct(productMapper.convertToEntity(dto.getProductDto()));
+        return productInBranch;
     }
 
     @Override
     public ProductInBranchDto convertToDto(ProductInBranch entity) {
-        return null;
+        return ProductInBranchDto.builder().id(entity.getId()).
+                quantity(entity.getQuantity()).
+                date(entity.getDate()).
+                branchDto(branchMapper.convertToDto(entity.getBranch())).
+                productDto(productMapper.convertToDto(entity.getProduct())).
+                build();
     }
 }
