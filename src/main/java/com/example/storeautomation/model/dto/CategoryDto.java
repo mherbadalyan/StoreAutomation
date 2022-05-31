8f2078b20ff5a297ba0f2fb743281df49eca0d39
TigranTigranyan <T.tigranyan1993@mail.ru>
2022-05-31 12:28:38 +0400

    add jsonProperty

--- src/main/java/com/example/storeautomation/model/dto/CategoryDto.java
+++ src/main/java/com/example/storeautomation/model/dto/CategoryDto.java
@@ -1,5 +1,6 @@
 package com.example.storeautomation.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,7 +9,7 @@ import lombok.Data;
 @AllArgsConstructor
 @Builder
 public class CategoryDto {
-
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
     private String categoryName;
